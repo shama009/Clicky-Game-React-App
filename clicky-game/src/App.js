d92aa226b4e2cedec3634a196b23788ee82ef875
Shama <shamaanjum09@outlook.com>
2018-02-04 13:16:12 -0800

    app.js render and shuffle methods updated

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -3,16 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    // Setting this.state.friends to the friends json array
+    state = {
+      cards
+    };
+
+    shufflePicture = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    //const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    //this.setState({ friends });
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Clicky Game!!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <strong>Click on an image to earn points, but don't click on any, more than once!</strong>
         </p>
+        <Wrapper>
+        {this.state.cards.map(picture => (
+          <pictureCard
+            shufflePicture={this.shufflePicture}
+            id={picture.id}
+            key={picture.id} // to get rid of unique key prop warning
+            name={picture.name}
+            image={picture.image}
+          />
+        ))}
+      </Wrapper>
       </div>
     );
   }
