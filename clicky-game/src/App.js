fa3a0b0378709d9545132f28a723b77f8e877413
Shama <shamaanjum09@outlook.com>
2018-02-05 10:08:03 -0800

    score logic

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -11,39 +11,35 @@ class App extends Component {
     cards,
     clickedArray: [],
     topScore: 0,
-    score: 0,
-    topScoresArray: [0]
+    score: 0
   };
   clickPicture = id => {
-    const max = Math.max(...this.state.topScoresArray);
-    console.log(max);
-    this.setState({topScore: max});
+    console.log(this.state.clickedArray)
     // Arrange the pictures in a random manner
+    const shuffledArray = this.shuffleArray(cards);
+    this.setState({cards: shuffledArray});
+    // if clicked an image already clicked set this.state.score = 0; empty clickeadArray, end of if block
     if (this.state.clickedArray.includes(id)) {
-      this.setState({ score: 0 ,
-      topScoresArray: this.state.topScoresArray.concat([this.state.topScore]),
-    });
-      console.log(this.state.topScoresArray, this.state.topScore);
+      this.setState({ score: 0, clickedArray: [] });
     }
     else {
-      // if (this.state.score >= this.state.topScore) {
-      //   this.setState({ topScore: this.state.score });
-      // }
       this.setState({
         clickedArray: this.state.clickedArray.concat([id]),
-        score: this.state.score + 1,
-        topScore: this.state.topScore + 1
+        score: this.state.score + 1
       });
-      //this.setState({ score: this.state.score + 1 });
-      //this.setState({topScore: this.state.topScore + 1});
-      //console.log(this.state.clickedArray, this.state.score, this.state.topScore);
     }
-    // const cardsClicked = this.state.cards.filter(picture => picture.id === id);
-    //console.log(this.state.clickedArray, this.state.score, this.state.topScore);
-
-    // Set this.state.cards equal to the new friends array
-    //this.setState({ cards });
-  };
+    // set topscore = score if score>topscore.
+    if (this.state.score > this.state.topScore) {
+      this.setState({ topScore: this.state.score });
+    }
+  }
+  shuffleArray = (picturesArray) => {
+      for (let i = picturesArray.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [picturesArray[i], picturesArray[j]] = [picturesArray[j], picturesArray[i]];
+      }
+      return picturesArray;
+  }
   render() {
     return (
       <div className="App">
